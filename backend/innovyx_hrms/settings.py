21fa8b2c83a049c1d63cb1e86c45169bd47bb9d0
vivekpratapsingh8700 <139740196+vivekpratapsingh8700@users.noreply.github.com>
2026-04-17 12:47:18 +0530

    updated email

--- backend/innovyx_hrms/settings.py
+++ backend/innovyx_hrms/settings.py
@@ -207,8 +207,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'servicescc.002@gmail.com'
-EMAIL_HOST_PASSWORD = 'pobtshsdfqdehswu'
+EMAIL_HOST_USER = 'alerts@innovyxtechlabs.com'
+EMAIL_HOST_PASSWORD = 'ebrhnknvfngrloum'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
