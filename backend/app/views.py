24f068d19a669df233027a6f408fda1b6bf45358
vivekpratapsingh8700 <139740196+vivekpratapsingh8700@users.noreply.github.com>
2026-07-06 12:01:05 +0530

    Update views.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -6175,7 +6175,8 @@ class GoogleLoginAPIView(APIView):
             idinfo = id_token.verify_oauth2_token(
                 token, 
                 google_requests.Request(), 
-                settings.GOOGLE_CLIENT_ID
+                settings.GOOGLE_CLIENT_ID,
+                clock_skew_in_seconds=10
             )
             
             email = (idinfo.get("email") or "").strip().lower()
